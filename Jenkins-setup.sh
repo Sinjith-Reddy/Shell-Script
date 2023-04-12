df9ec698f5ae2438ce18b80589be691873928b82
Sinjith Reddy <58818193+Sinjith-Reddy@users.noreply.github.com>
2023-04-12 23:12:53 +0530

    jenkins changed rpm key, so updated

--- Jenkins-setup.sh
+++ Jenkins-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install java-11-openjdk -y
 curl -L -o /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install  epel-release -y
 yum install jenkins -y
 systemctl enable jenkins
