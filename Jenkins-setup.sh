6c08c098d1a0fa0d254bd329352bf6c2d8b77926
Sinjith Reddy <58818193+Sinjith-Reddy@users.noreply.github.com>
2023-04-13 06:57:32 +0530

    Delete Jenkins-setup.sh

--- Jenkins-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum install java-11-openjdk -y
-curl -L -o /etc/yum.repos.d/jenkins.repo  https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install  epel-release -y
-yum install jenkins -y
-systemctl enable jenkins
-systemctl start jenkins
